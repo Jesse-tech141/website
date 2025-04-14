f4f320f44f4d5996695c381192ad3f4463255722
Jesse Okereke <jessechimeremeze@gmail.com>
2025-04-14 11:26:32 +0100

    changed example.test.js

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -1,14 +1,7 @@
-let testData;
-
-beforeEach(() => {
-  testData = { value: 10 };
-});
-
-afterEach(() => {
-  testData = null;
-});
-
-test('test with setup/teardown', () => {
-  testData.value += 5;
-  expect(testData.value).toBe(15);
+test('object snapshot', () => {
+  const config = {
+    env: 'test',
+    timeout: 5000
+  };
+  expect(config).toMatchSnapshot();
 });
